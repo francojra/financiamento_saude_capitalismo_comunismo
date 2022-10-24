9cfa06b7c30148ccac5217704d9d331c1836bacb
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-23 23:28:12 -0300

    Add files via upload

---
+++ financiamento_saude_paises_comunistas_capitalistas.R
@@ -0,0 +1,37 @@
+
+# Financiamento em serviços de saúde -------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 23/10/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/financing-healthcare ------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Boa saúde é parte chave para nossa qualidade de vida. Nesse registro nós focamos
+### sobre os investimentos em saúde - um dos mais importantes contributos para proteger
+### e melhorar a saúde. 
+
+### Os cuidados de saúde com financiamento público são um legado da era das luzes. Os primeiros
+### exemplos sobre seguros de saúde datam do final do século 19. Dados desses primeiros
+### sistemas mostram que gastos com saúde apenas começaram a aumentar vários anos após
+### a expansão do seguro saúde, com a descobertas de novos tratamentos.
+
+### Os impactos que os desenvolvimentos científicos tiveram sobre gastos em saúde é símbolo
+### nos Estados Unidos: em décadas recentes, como a possibilidade de tratamentos expandiram 
+### rapidamente, gastos com saúde aumentaram - tanto no privado, quanto no público, tanto
+### per capita como em taxa de produto interno bruto. Isso ocorreu sem grande mudanças na
+### cobertura dos seguro de saúde, e teve duas consequências importantes: (i) os Estados Unidos
+### atualmente gasta mais dinheiro do governo sobre saúde por pessoa que muitos países com 
+### programas universais; (ii) a despesa é tão concentrada que o 1% de topo dos gastadores 
+### representa mais de 20% da despesa total com cuidados de saúde.
+
+### As despesas globais nos cuidados com saúde como parte do rendimento mundial tem aumentado,
+### continuamente mas lentamente nas duas últimas décadas. Entretanto, existe no mundo substancial
+### heterogeneidade. Regionalmente, países de alta renda gastam - e vem gastando - uma taxa 
+### muito maior da renda que países mais pobres (cerca de duas vezes mais). Além disso, em
+### contraste aos países de alta renda, os países de renda média e baixa apresentam menor financiamento
+### público em saúde - embora tenham vindo a recuperar seu atraso - e o papel das despesas é muito
+### mais alto (cerca de 50% dos gastos totais).
+
+
+
+
